df5ec45d02f0119c8c3a5d4753c31d84b91ebb7c
VishalGithub11 <modanwalvishal@gmail.com>
2022-11-19 20:12:25 +0530

    manifest-changes

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,11 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>airgram</h1>
+      <div style={{display:"flex"}}>
+      
+      <h1>airg </h1> <span><img src={process.env.PUBLIC_URL + '/fish-icon.png'} width="50px" height={"50px"} /></span> <h1> am</h1>
+      </div>
+    
       <h2>Your Pictures</h2>
       <p>Upload any images you like in png, jpeg or jpg format.</p>
     </div>
